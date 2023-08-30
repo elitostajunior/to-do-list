aad9350f3f99ac6f72bf2f7270af57897648d077
eligtj <eli_gtj@hotmail.com>
2023-08-29 23:03:35 -0300

    Feito endpoint "criar tarefa"

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { AddressInfo } from "net";
 import createUser from "./endpoints/createUser";
 import getUserById from "./endpoints/getUserById";
 import editUser from "./endpoints/editUser";
+import createTask from "./endpoints/createTask";
 
 dotenv.config();
 
@@ -27,6 +28,8 @@ app.use(cors());
 app.put('/user', createUser)
 app.get('/user/:id', getUserById)
 app.post('/user/edit/:id', editUser)
+
+app.put('/task', createTask)
 
 const server = app.listen(process.env.PORT || 3003, () => {
    if (server) {
